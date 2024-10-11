cc1d1e258232fd1502dc407d9541640f3d7d6812
nagybrandy <57422699+nagybrandy@users.noreply.github.com>
2024-10-11 14:31:30 +0200

    0-1

---
+++ 00. alkalom/script.js
@@ -0,0 +1,13 @@
+
+import {add} from './math.js'
+import cowsay from "cowsay";
+console.log(add(5,6))
+console.log('Hello World!')
+
+let opts  = {
+    text: "Hello from Javascript!",
+    e: '',
+};
+
+console.log(cowsay.say(opts))
+
